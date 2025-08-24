d4069ef5ed44ccd52f425bfe78c2567d6066e25a
Graceee <gracesong146@gmail.com>
2025-08-25 01:16:30 +0800

    Completed Level-1

--- src/main/java/Cathy.java
+++ src/main/java/Cathy.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Cathy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
+        String[] goodbyes = {
+                "Finally! I was getting bored...",
+                "Leaving already? I barely started rolling my eyes.",
+                "Oh joy, another person abandoning me.",
+                "Bye then. Try not to miss me too much."
+        };
 
         String logo = "  ____      _   _          \n"
                 + " / ___|__ _| |_| |__  _   _ \n"
@@ -11,8 +23,21 @@ public class Cathy {
         System.out.println("____________________________________________");
         System.out.println(" Hello! I'm Cathy");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________");
+        System.out.println("____________________________________________");while (true) {
+            String userInput = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                int idx = rand.nextInt(goodbyes.length);
+                System.out.println(" " + goodbyes[idx]);
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println(" " + userInput);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
