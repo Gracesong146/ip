ef0ef8a0ca9f6ecaecd2babac659eb50f8f5a83c
Graceee <gracesong146@gmail.com>
2025-08-25 01:02:42 +0800

    Completed Level-0

---
+++ src/main/java/Cathy.java
@@ -0,0 +1,18 @@
+public class Cathy {
+    public static void main(String[] args) {
+
+        String logo = "  ____      _   _          \n"
+                + " / ___|__ _| |_| |__  _   _ \n"
+                + "| |   / _` | __| '_ \\| | | |\n"
+                + "| |__| (_| | |_| | | | |_| |\n"
+                + " \\____\\__,_|\\__|_| |_|\\__, |\n"
+                + "                       |___/ \n";
+        System.out.println(logo);
+        System.out.println("____________________________________________");
+        System.out.println(" Hello! I'm Cathy");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________");
+    }
+}
