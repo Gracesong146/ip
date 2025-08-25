2324a0df9d7cdf962ab7757ca86f0d3ae068f6ea
Graceee <gracesong146@gmail.com>
2025-08-25 22:06:02 +0800

    Add the ability to mark tasks as done and not done. Level 3.

--- src/main/java/Cathy.java
+++ src/main/java/Cathy.java
@@ -43,39 +43,41 @@ public class Cathy {
                 "Bye then. Try not to miss me too much."
         };
 
-        String logo = "  ____      _   _          \n"
-                + " / ___|__ _| |_| |__  _   _ \n"
-                + "| |   / _` | __| '_ \\| | | |\n"
-                + "| |__| (_| | |_| | | | |_| |\n"
-                + " \\____\\__,_|\\__|_| |_|\\__, |\n"
-                + "                       |___/ \n";
+        String logo =
+                  "      ____      _   _          \n"
+                + "     / ___|__ _| |_| |__  _   _ \n"
+                + "    | |   / _` | __| '_ \\| | | |\n"
+                + "    | |__| (_| | |_| | | | |_| |\n"
+                + "     \\____\\__,_|\\__|_| |_|\\__, |\n"
+                + "                           __| |\n"
+                + "                           |___/ \n";
         System.out.println(logo);
-        System.out.println("____________________________________________\n");
-        System.out.println(" Oh look, someone showed up.\n");
-        System.out.println(" I'm Cathy, your gloriously underappreciated task assistant.\n");
-        System.out.println(" Here's what I *might* help you with (if you're lucky):");
-        System.out.println(" - Type anything to add it to your to-do list.");
-        System.out.println(" - Type 'list' to see your glorious pile of tasks.");
-        System.out.println(" - Type 'mark [number]' to mark a task as done. Try not to mess it up.");
-        System.out.println(" - Type 'bye' to leave me in peace.");
-        System.out.println("____________________________________________\n");
+        System.out.println("    ____________________________________________\n");
+        System.out.println("     Oh look, someone showed up.\n");
+        System.out.println("     I'm Cathy, your gloriously underappreciated task assistant.\n");
+        System.out.println("     Here's what I *might* help you with (if you're lucky):");
+        System.out.println("     - Type anything to add it to your to-do list.");
+        System.out.println("     - Type 'list' to see your glorious pile of tasks.");
+        System.out.println("     - Type 'mark [number]' to mark a task as done. Try not to mess it up.");
+        System.out.println("     - Type 'bye' to leave me in peace.");
+        System.out.println("    ____________________________________________\n");
         while (true) {
             String userInput = scanner.nextLine();
-            System.out.println("____________________________________________________________\n");
+            System.out.println("    ____________________________________________________________\n");
 
             if (userInput.equalsIgnoreCase("bye")) {
                 int idx = rand.nextInt(goodbyes.length);
-                System.out.println(" " + goodbyes[idx]);
-                System.out.println("____________________________________________________________\n");
+                System.out.println("     " + goodbyes[idx]);
+                System.out.println("    ____________________________________________________________\n");
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
                 int idx = 1;
-                System.out.println("list:");
+                System.out.println("     list:");
                 for (Task item : toDoList) {
-                    System.out.println(idx + ". [" + item.getStatusIcon() + "] " + item);
+                    System.out.println("     " + idx + ". [" + item.getStatusIcon() + "] " + item);
                     idx += 1;
                 }
-                System.out.println("____________________________________________________________\n");
+                System.out.println("    ____________________________________________________________\n");
             } else if (userInput.toLowerCase().startsWith("mark")) {
                 String[] parts = userInput.split(" ");
                 if (parts.length == 2) {
@@ -83,21 +85,26 @@ public class Cathy {
                         int taskNumber = Integer.parseInt(parts[1]);
                         Task newT = toDoList.get(taskNumber - 1);
                         if (newT.getStatusIcon().equals("X")) {
-                            System.out.println("Darling, that task’s already done. No need to be an overachiever.");
-                            System.out.println("____________________________________________________________\n");
+                            System.out.println("     Darling, that task’s already done. No need to be an overachiever.");
+                            System.out.println("    ____________________________________________________________\n");
                         } else {
                             newT.markAsDone();
                             toDoList.set(taskNumber - 1, newT);
+                            System.out.println("     Nice! I've marked this task as done:");
+                            System.out.println("        [X] " + newT);
+                            System.out.println("    ____________________________________________________________\n");
                         }
                     } catch (NumberFormatException e) {
-                        System.out.println("Sweetie, numbers only. This isn’t a spelling bee");
-                        System.out.println("____________________________________________________________\n");
+                        System.out.println("     Sweetie, numbers only. This isn’t a spelling bee");
+                        System.out.println("    ____________________________________________________________\n");
                     } catch (IndexOutOfBoundsException e) {
-                        System.out.println("Nice try, but that task doesn't even exist.");
-                        System.out.println("____________________________________________________________\n");
+                        System.out.println("     Nice try, but that task doesn't even exist.");
+                        System.out.println("    ____________________________________________________________\n");
                     }
                 } else {
-                    System.out.println("If you're going to mark something, at least give me a valid number. I'm not psychic");
+                    System.out.println("     If you're going to mark something, at least give me something valid.");
+                    System.out.println("     Give it in the following form: mark [number]");
+                    System.out.println("    ____________________________________________________________\n");
                 }
             } else if (userInput.toLowerCase().startsWith("unmark")) {
                 String[] parts = userInput.split(" ");
@@ -106,28 +113,33 @@ public class Cathy {
                         int taskNumber = Integer.parseInt(parts[1]);
                         Task newT = toDoList.get(taskNumber - 1);
                         if (newT.getStatusIcon().equals(" ")) {
-                            System.out.println("Task " + taskNumber + " is already unmarked. Stop trying to double negative your way through life.");
-                            System.out.println("____________________________________________________________\n");
+                            System.out.println("     Task " + taskNumber + " is already unmarked.");
+                            System.out.println("Stop trying to double negative your way through life.");
+                            System.out.println("    ____________________________________________________________\n");
                         } else {
                             newT.markAsNotDone();
                             toDoList.set(taskNumber - 1, newT);
+                            System.out.println("     OK, I've marked this task as not done yet:");
+                            System.out.println("        [ ] " + newT);
+                            System.out.println("    ____________________________________________________________\n");
                         }
                     } catch (NumberFormatException e) {
-                        System.out.println("Sweetie, numbers only. This isn’t a spelling bee");
-                        System.out.println("____________________________________________________________\n");
+                        System.out.println("     Sweetie, numbers only. This isn’t a spelling bee");
+                        System.out.println("    ____________________________________________________________\n");
                     } catch (IndexOutOfBoundsException e) {
-                        System.out.println("Nice try, but that task doesn't even exist.");
-                        System.out.println("____________________________________________________________\n");
+                        System.out.println("     Nice try, but that task doesn't even exist.");
+                        System.out.println("    ____________________________________________________________\n");
                     }
                 } else {
-                    System.out.println("If you're going to mark something, at least give me a valid number. I'm not psychic");
+                    System.out.println("     If you're going to mark something, at least give me a valid number. I'm not psychic");
+                    System.out.println("    ____________________________________________________________\n");
                 }
                 
             } else {
                 Task t = new Task(userInput);
                 toDoList.add(t);
-                System.out.println("added: " + userInput);
-                System.out.println("____________________________________________________________\n");
+                System.out.println("     added: " + userInput);
+                System.out.println("    ____________________________________________________________\n");
             }
         }
 
