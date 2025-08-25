c4854576b7e9c9e7f32f5b891b505a683017c3b8
Graceee <62082817+Gracesong146@users.noreply.github.com>
2025-08-25 10:55:30 +0800

    Add toDoList functionality and improve output formatting

--- src/main/java/Cathy.java
+++ src/main/java/Cathy.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Cathy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
-
+        ArrayList<String> toDoList = new ArrayList<>();
+        
         String[] goodbyes = {
                 "Finally! I was getting bored...",
                 "Leaving already? I barely started rolling my eyes.",
@@ -20,21 +22,31 @@ public class Cathy {
                 + " \\____\\__,_|\\__|_| |_|\\__, |\n"
                 + "                       |___/ \n";
         System.out.println(logo);
-        System.out.println("____________________________________________");
+        System.out.println("____________________________________________\n");
         System.out.println(" Hello! I'm Cathy");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________");while (true) {
+        System.out.println("____________________________________________\n");
+        while (true) {
             String userInput = scanner.nextLine();
-            System.out.println("____________________________________________________________");
+            System.out.println("____________________________________________________________\n");
 
             if (userInput.equalsIgnoreCase("bye")) {
                 int idx = rand.nextInt(goodbyes.length);
                 System.out.println(" " + goodbyes[idx]);
-                System.out.println("____________________________________________________________");
+                System.out.println("____________________________________________________________\n");
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                int idx = 1;
+                System.out.println("list:");
+                for (String item : toDoList) {
+                    System.out.println(idx + ". " + item);
+                    idx += 1;
+                }
+                System.out.println("____________________________________________________________\n");
             } else {
-                System.out.println(" " + userInput);
-                System.out.println("____________________________________________________________");
+                toDoList.add(userInput);
+                System.out.println("added: " + userInput);
+                System.out.println("____________________________________________________________\n");
             }
         }
 
