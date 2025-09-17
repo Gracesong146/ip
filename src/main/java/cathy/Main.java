26e98081aab7ec46b4e2d4d14a868a408faa3bf7
Graceee <gracesong146@gmail.com>
2025-09-17 14:00:27 +0800

    Use AI Assistance to add title and app icon

--- src/main/java/cathy/Main.java
+++ src/main/java/cathy/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -23,6 +24,13 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+
+            // Set window title
+            stage.setTitle("Cathy - Your Underappreciated Task Assistant");
+
+            // Set custom icon (for title bar + taskbar/dock)
+            stage.getIcons().add(new Image(Main.class.getResourceAsStream("/images/cathy_pfp.png")));
+
             stage.setScene(scene);
             stage.setMinHeight(MIN_HEIGHT);
             stage.setMinWidth(MIN_WIDTH);
